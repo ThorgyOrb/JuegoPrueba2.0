7e5f0228573ab222d0e1eef77c36ed78f63fd96c
Martinez Tovar Edgar Ulises <170940@upslp.edu.mx>
2023-02-13 03:50:00 -0600

    2.0

--- Assets/Scripts/CubeGenerator.cs
+++ Assets/Scripts/CubeGenerator.cs
@@ -7,15 +7,20 @@ public class CubeGenerator : MonoBehaviour
     public GameObject[] objects;
     public float spawnTime = 1.0f;
     public float spawnDelay = 0.5f;
+    int spawnNumber = 280;
+    //create an foat array
+    float[] positions = {-7.0f, -5.8f, -4.6f, -3.4f, -2.2f, -1.0f, 0.2f, 1.4f, 2.6f, 3.8f};
     void Start(){
         InvokeRepeating("SpawnCube", spawnDelay, spawnTime);
     } 
     void SpawnCube(){
+        //change the x position of the object randomly using the array
+        transform.position = new Vector3(positions[Random.Range(0, positions.Length)], transform.position.y, transform.position.z);
         int spawnPointIndex = Random.Range(0, objects.Length);
         Instantiate(objects[spawnPointIndex], transform.position, transform.rotation);
         //repeat 5 times and then stop
-        if (spawnTime >= 0.6f){
-            spawnTime -= 0.1f;
+        if (spawnNumber >= 0){
+            spawnNumber -= 1;
         }
         else{
             CancelInvoke("SpawnCube");
--- Assets/Scripts/MovementCode.cs
+++ Assets/Scripts/MovementCode.cs
@@ -13,8 +13,17 @@ public class MovementCode : MonoBehaviour
     private void Update() {
         //Translate the object in -z direction
         transform.Translate(0, 0, -UncleSpeed * Time.deltaTime);
-        
-
-
+        //Stop the translation when the object reach -z=7
+        if (transform.position.z < -7) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -7);
+        }
     }
+    //Detect collision with the object
+    private void OnCollisionEnter(Collision collision) {
+        //If the object collides with anything stop the translation
+        if (collision.gameObject.tag == "Character") {
+            UncleSpeed = 0;
+        }
+    }
+
 }
